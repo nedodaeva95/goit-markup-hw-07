131404819ce06d8406f38063b0ea32de69bc169b
nedodaeva95 <nedodaeva21@gmail.com>
2023-03-11 00:46:27 +0100

    hw-7

--- js/menu.js
+++ js/menu.js
@@ -6,7 +6,7 @@
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeModalBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-open");
